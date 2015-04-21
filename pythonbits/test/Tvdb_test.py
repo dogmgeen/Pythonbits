c41cb7e14fc430aac684500ef6b55e8d011ed72c
burkean <cristo@sdf.org>
2015-04-21 16:07:15 +0100

    Updated tests for Tvdbparser adapter
    Replaced tests for Tvdb_test.py with ones that test the interface instead of
    testing if tvdb_api objects contain the keys and values they contained from
    the time the tests were first written.
    
    Also they are no longer dependent on the network.

--- pythonbits/test/Tvdb_test.py
+++ pythonbits/test/Tvdb_test.py
@@ -7,68 +7,118 @@ Created by Ichabond on 2012-07-03.
 """
 
 import unittest
+from nose.tools import assert_raises, raises
+
+import mock
+import tvdb_api
+
 from pythonbits.TvdbParser import TVDB
 
 
-class TvdbTest(unittest.TestCase):
-    def setUp(self):
-        self.maxDiff = None
-        self.tvdb = TVDB()
-        self.Episode = self.tvdb.search("Burn Notice", episode="S06E01")
-        self.Season = self.tvdb.search("Burn Notice", season=6)
-        self.Show = self.tvdb.search("Scrubs")
+def test_invalid_episode_identifier_causes_exit():
+    tv = TVDB()
+    tv.tvdb = None
 
-    def testEpisode(self):
-        self.assertEqual(self.Episode["episodename"], "Scorched Earth")
-        self.assertEqual(self.Episode["director"], "Stephen Surjik")
-        self.assertEqual(self.Episode['firstaired'], "2012-06-14")
-        self.assertEqual(self.Episode['writer'], "Matt Nix")
-        self.assertTrue(len(self.Episode) > 26)
+    eps = ("S06 E01","S0601", "601", "S10", "SE10")
+    for e in eps:
+        assert_raises(SystemExit, tv.search, "Burn Notice", episode=e)
+    assert_raises(TypeError, tv.search, "Burn Notice", episode="S01E01")  # valid
 
 
-    def testSeason(self):
-        self.assertTrue(len(self.Season) > 10)
+def test_search_for_show_returns_underlying_show():
+    # mock out the underlying api call to tvdb_api
+    tv = TVDB()
+    tv.tvdb = {'Scrubs': tvdb_api.Show()}
 
-    def testShow(self):
-        self.assertTrue(len(self.Show.data) > 25)
-        self.assertEqual(len(self.Show), 10)
-        self.assertIn(self.Show['network'], ["ABC", "NBC"])
-        self.assertEqual(self.Show['seriesname'], "Scrubs")
+    assert isinstance(tv.search("Scrubs"), tvdb_api.Show)
+    
 
-    def testSummaries(self):
-        self.tvdb.search("Burn Notice", episode="S06E01")
-        BurnNoticeS06E01 = {'director': u'Stephen Surjik', 'rating': u'7.9',
-                            'aired': u'2012-06-14', 'language': u'en',
-                            'title': u'Scorched Earth', 'genre': u'|Action and Adventure|',
-                            'summary': u'Michael pursues Anson in Miami. Meanwhile, Fiona is taken into custody and interrogated by a former foe.',
-                            'writer': u'Matt Nix',
-                            'url': u'http://thetvdb.com/?tab=episode&seriesid=80270&seasonid=483302&id=4246443',
-                            'series': u'Burn Notice'}
-        summary = self.tvdb.summary()
-        for key in BurnNoticeS06E01:
-            self.assertIn(key, summary)
+def test_search_for_episode_returns_underlying_episode():
+    # mock out the underlying api call to tvdb_api
+    tv = TVDB()
+    tv.tvdb = {'Burn Notice': {6: {1: tvdb_api.Episode()}}}
+
+    assert isinstance(tv.search("Burn Notice", episode="S06E01"), tvdb_api.Episode)
 
 
-        self.tvdb.search("Burn Notice", season=5)
-        BurnNoticeS05 = {'episode11': u'Better Halves', 'episode17': u'Acceptable Loss',
-                         'episode15': u'Necessary Evil', 'episode12': u'Dead to Rights',
-                         'episode13': u'Damned If You Do', 'episode3': u'Mind Games',
-                         'episode1': u'Company Man', 'episode10': u'Army of One', 'episodes': 18,
-                         'episode2': u'Bloodlines', 'episode5': u'Square One', 'episode4': u'No Good Deed',
-                         'episode7': u'Besieged', 'episode6': u'Enemy of My Enemy',
-                         'episode9': u'Eye for an Eye', 'episode8': u'Hard Out', 'episode18': u'Fail Safe',
-                         'episode16': u'Depth Perception', 'episode14': u'Breaking Point',
-                         'url': u'http://thetvdb.com/?tab=season&seriesid=80270&seasonid=463361',
-                         'series': u'Burn Notice',
-                         'summary': u'Covert intelligence operative Michael Westen has been punched, kicked, choked and shot. And now he\'s received a "burn notice", blacklisting him from the intelligence community and compromising his very identity. He must track down a faceless nemesis without getting himself killed while doubling as a private investigator on the dangerous streets of Miami in order to survive.'}
-        summary = self.tvdb.summary()
-        for key in BurnNoticeS05:
-            self.assertIn(key, summary)
+def test_search_for_season_returns_underlying_season():
+    # mock out the underlying api call to tvdb_api
+    tv = TVDB()
+    tv.tvdb = {'Burn Notice': {6: tvdb_api.Season()}}
 
-        self.tvdb.search("Scrubs")
-        Scrubs = {'rating': u'9.0', 'network': u'ABC', 'series': u'Scrubs', 'contentrating': u'TV-PG',
-                  'summary': u'Scrubs focuses on the lives of several people working at Sacred Heart, a teaching hospital. It features fast-paced dialogue, slapstick, and surreal vignettes presented mostly as the daydreams of the central character, Dr. John Michael "J.D." Dorian.',
-                  'seasons': 10, 'url': u'http://thetvdb.com/?tab=series&id=76156'}
-        summary = self.tvdb.summary()
-        for key in Scrubs:
-            self.assertIn(key, summary)
+    assert isinstance(tv.search("Burn Notice", season=6), tvdb_api.Season)
+
+
+def test_expected_keys_in_show_summary():
+    tv = TVDB()
+    tv.episode = None
+    tv.season = None
+    tv.show = mock_show_builder()
+
+    summary = tv.summary()
+    expected = ('series', 'seasons', 'network', 'rating', 'contentrating',
+                'summary', 'url')
+    for k in expected:
+        assert k in summary
+
+
+def mock_show_builder():
+    show = tvdb_api.Show()
+    show['seriesname'] = ''
+    show['overview'] = ''
+    show.__len__ = lambda self:  5
+    show['network'] = 'ABC'
+    show['rating'] = ''
+    show['summary'] = ''
+    show['contentrating'] = ''
+    show['id'] = ''
+    return show
+
+
+def mock_episode_builder():
+    episode = tvdb_api.Episode()
+    episode['episodename'] = ''
+    episode['director'] = ''
+    episode['firstaired'] = ''
+    episode['writer'] = ''
+    episode['rating'] = ''
+    episode['overview'] = ''
+    episode['language'] = ''
+    episode['seriesid'] = '1'
+    episode['genre'] = ''
+    episode['seasonid'] = ''
+    episode['id'] = ''
+    return episode
+
+
+def mock_season_builder():
+    season = tvdb_api.Season()
+    season['overview'] = ''
+    season.episodes = [mock_episode_builder(), mock_episode_builder()]
+
+
+def test_expected_keys_in_episode_summary():
+    tv = TVDB()
+    tv.show = mock_show_builder()
+    tv.season = None
+    tv.episode = mock_episode_builder()
+    tv.tvdb = {1: {'genre': ''}}
+
+    summary = tv.summary()
+    expected = ('title', 'director', 'aired', 'writer', 'rating', 'summary', 'language',
+                'url', 'genre', 'series', 'seriessummary')
+    for k in expected:
+        assert k in summary
+
+
+def test_expected_keys_in_season_summary():
+    tv = TVDB()
+    tv.show = mock_show_builder()
+    tv.season = mock_season_builder()
+    tv.episode = None
+
+    summary = tv.summary()
+    expected = ('series', 'url', 'summary')
+    # doesn't test for the presence of episode\d keys
+    for k in expected:
+        assert k in summary, k
